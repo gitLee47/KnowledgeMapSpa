936c17667a001fca1669bd6700c00fffd949e13e
Liju Robin George <liju.leelives@gmail.com>
2018-09-06 19:49:20 -0400

    added routing

--- KnowledgeMapSpa/src/app/home/home.module.ts
+++ KnowledgeMapSpa/src/app/home/home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapComponent } from './map/map.component';
+import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HomeRoutingModule
   ],
   declarations: [MapComponent]
 })
