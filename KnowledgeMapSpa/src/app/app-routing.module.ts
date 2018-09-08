b92ef9ca56acf14d6594b2530be87a647289d3fe
Liju Robin George <liju.leelives@gmail.com>
2018-09-08 12:40:21 -0400

    updated routes and map html

--- KnowledgeMapSpa/src/app/app-routing.module.ts
+++ KnowledgeMapSpa/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { ErrorPageComponent } from './common/error-page/error-page.component';
 
 const routes: Routes = [
-    { path:  '', redirectTo:  '/map', pathMatch:  'full' },
+    { path:  '', loadChildren:  './home/home.module#HomeModule', pathMatch:  'full' },
     { path:  '**', component:  ErrorPageComponent }
 ];
 
