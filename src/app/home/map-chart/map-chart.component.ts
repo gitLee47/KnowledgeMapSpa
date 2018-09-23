b80134f168aeb97e3d757dfe590716d66d08f325
Liju Robin George <liju.leelives@gmail.com>
2018-09-23 13:08:17 -0400

    added auto indent and button focus class

--- src/app/home/map-chart/map-chart.component.ts
+++ src/app/home/map-chart/map-chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ChangeDetectionStrategy, ElementRef, Input, OnChanges, ViewChild} from '@angular/core';
+import { ChangeDetectionStrategy, ElementRef, Input, OnChanges, ViewChild } from '@angular/core';
 
 import * as d3 from 'd3';
 
@@ -25,12 +25,12 @@ export class MapChartComponent implements OnInit, OnChanges {
   private chartProps: any;
 
   private jsonData: any;
-  private population : any;
+  private population: any;
   private countriesGroup: any;
   private path: any;
   private projection: any;
   private color: any;
-  
+
   constructor(private homeService: HomeService) { }
 
   ngOnInit() {
@@ -39,17 +39,17 @@ export class MapChartComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges() {
-    if(this.countriesGroup)
+    if (this.countriesGroup)
       this.getJsonData(this.topic);
   }
 
   getJsonData(topic: string) {
-    if(!topic) {
+    if (!topic) {
       return [];
     }
 
     this.homeService.getTopicCount(topic).subscribe(populationData => {
-      if(populationData) {
+      if (populationData) {
         this.population = populationData;
         this.jsonData = JSON.parse(countryData);
         this.buildChart();
@@ -59,177 +59,175 @@ export class MapChartComponent implements OnInit, OnChanges {
 
   setupChart() {
     var margin = { top: 30, right: 20, bottom: 30, left: 50 },
-    width = 1000,
-    height = 550;
+      width = 1000,
+      height = 550;
 
     // Define map projection
     this.projection = d3
-    .geoEquirectangular()
-    .center([0, 15]) // set centre to further North
-    .scale(width/(2*Math.PI)) // scale to fit group width
-    .translate([width/2, height/2]); // ensure centred in group
-    
+      .geoEquirectangular()
+      .center([0, 15]) // set centre to further North
+      .scale(width / (2 * Math.PI)) // scale to fit group width
+      .translate([width / 2, height / 2]); // ensure centred in group
+
     this.path = d3
-    .geoPath()
-    .projection(this.projection);
+      .geoPath()
+      .projection(this.projection);
 
     var zoom = d3
-    .zoom()
-    .on("zoom", ()=> {
-      var t = d3
-      .event
-      .transform;
+      .zoom()
+      .on("zoom", () => {
+        var t = d3
+          .event
+          .transform;
 
-      this.countriesGroup.attr(
-        "transform","translate(" + [t.x, t.y] + ")scale(" + t.k + ")"
-      );
-    });
+        this.countriesGroup.attr(
+          "transform", "translate(" + [t.x, t.y] + ")scale(" + t.k + ")"
+        );
+      });
 
     var svg = d3.select(this.chartElement.nativeElement)
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height)
-    .call(zoom);
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .call(zoom);
 
     this.countriesGroup = svg
-    .append("g")
-    .attr("id", "map")
-    .attr("class", "container");
+      .append("g")
+      .attr("id", "map")
+      .attr("class", "container");
   }
 
   buildChart() {
     var colorIndex = Math.floor(Math.random() * colors.length);
     this.color = d3.scaleThreshold<number, string>()
-    .domain([
-      10000,
-      100000,
-      500000,
-      1000000,
-      5000000,
-      10000000,
-      50000000,
-      100000000,
-      500000000,
-      1500000000
-    ])
-    .range(colors[colorIndex]);
-  
+      .domain([
+        10000,
+        100000,
+        500000,
+        1000000,
+        5000000,
+        10000000,
+        50000000,
+        100000000,
+        500000000,
+        1500000000
+      ])
+      .range(colors[colorIndex]);
+
     d3.select("g")
-    .selectAll("path")
-    .remove();
+      .selectAll("path")
+      .remove();
 
     d3.selectAll('.countryLabel')
-    .remove();
+      .remove();
 
     const populationById = {};
     this.population.forEach(d => { populationById[d.id] = +d.population; });
     this.jsonData.features.forEach(d => { d.population = populationById[d.id] });
 
     var countries = this.countriesGroup
-    .selectAll("path")
-    .data(this.jsonData.features)
-    .enter()
-    .append("path")
-    .attr("d", this.path)
-    .style('fill', d => this.color(populationById[d.id]))
-    .attr("id", function(d, i) {
-      return "country" + d.id;
-    })
-    .attr("class", "country")
-    // add a mouseover action to show name label for feature/country
-    .on("mouseover", function(d, i) {
-      d3.select("#countryLabel" + d.id).style("display", "block");
-    })
-    .on("mouseout", function(d, i) {
-      d3.select("#countryLabel" + d.id).style("display", "none");
-    })
-    // add an onclick action to zoom into clicked country
-    .on("click", function(d, i) {
-      d3.selectAll(".country").classed("country-on", false);
-      d3.select(this).classed("country-on", true);
-      //boxZoom(path.bounds(d), path.centroid(d), 20);
-    });
+      .selectAll("path")
+      .data(this.jsonData.features)
+      .enter()
+      .append("path")
+      .attr("d", this.path)
+      .style('fill', d => this.color(populationById[d.id]))
+      .attr("id", function (d, i) {
+        return "country" + d.id;
+      })
+      .attr("class", "country")
+      // add a mouseover action to show name label for feature/country
+      .on("mouseover", function (d, i) {
+        d3.select("#countryLabel" + d.id).style("display", "block");
+      })
+      .on("mouseout", function (d, i) {
+        d3.select("#countryLabel" + d.id).style("display", "none");
+      })
+      // add an onclick action to zoom into clicked country
+      .on("click", function (d, i) {
+        d3.selectAll(".country").classed("country-on", false);
+        d3.select(this).classed("country-on", true);
+        //boxZoom(path.bounds(d), path.centroid(d), 20);
+      });
 
     var self = this;
     var countryLabels = this.countriesGroup
-    .selectAll("g")
-    .data(this.jsonData.features)
-    .enter()
-    .append("g")
-    .attr("class", "countryLabel")
-    .attr("id", function(d) {
-      return "countryLabel" + d.id;
-    })
-    .attr("transform", function(d) {
-      return (
-        "translate(" + self.path.centroid(d)[0] + "," + self.path.centroid(d)[1] + ")"
-      );
-    })
-    // add mouseover functionality to the label
-    .on("mouseover", function(d, i) {
-      d3.select(this).style("display", "block");
-    })
-    .on("mouseout", function(d, i) {
-      d3.select(this).style("display", "none");
-    })   
-    // add an onlcick action to zoom into clicked country
-    .on("click", function(d, i) {
-      d3.selectAll(".country").classed("country-on", false);
-      d3.select("#country" + d.id).classed("country-on", true);
-      //boxZoom(path.bounds(d), path.centroid(d), 20);
-    });
+      .selectAll("g")
+      .data(this.jsonData.features)
+      .enter()
+      .append("g")
+      .attr("class", "countryLabel")
+      .attr("id", function (d) {
+        return "countryLabel" + d.id;
+      })
+      .attr("transform", function (d) {
+        return (
+          "translate(" + self.path.centroid(d)[0] + "," + self.path.centroid(d)[1] + ")"
+        );
+      })
+      // add mouseover functionality to the label
+      .on("mouseover", function (d, i) {
+        d3.select(this).style("display", "block");
+      })
+      .on("mouseout", function (d, i) {
+        d3.select(this).style("display", "none");
+      })
+      // add an onlcick action to zoom into clicked country
+      .on("click", function (d, i) {
+        d3.selectAll(".country").classed("country-on", false);
+        d3.select("#country" + d.id).classed("country-on", true);
+        //boxZoom(path.bounds(d), path.centroid(d), 20);
+      });
 
     countryLabels
-    .append("text")
-    .attr("class", "countryName")
-    .style("text-anchor", "middle")
-    .attr("dx", 0)
-    .attr("dy", 0)
-    .text(function(d) {
-      return d.properties.name;
-    })
-    .call(this.getTextBox);
+      .append("text")
+      .attr("class", "countryName")
+      .style("text-anchor", "middle")
+      .attr("dx", 0)
+      .attr("dy", 0)
+      .text(function (d) {
+        return d.properties.name;
+      })
+      .call(this.getTextBox);
 
     // add a background rectangle the same size as the text
     countryLabels
-    .insert("rect", "text")
-    .attr("class", "countryLabelBg")
-    .attr("transform", function(d) {
-      return "translate(" + (d.bbox.x - 2) + "," + d.bbox.y + ")";
-    })
-    .attr("width", function(d) {
-      return d.bbox.width + 4;
-    })
-    .attr("height", function(d) {
-      return d.bbox.height;
-    });     
-    
+      .insert("rect", "text")
+      .attr("class", "countryLabelBg")
+      .attr("transform", function (d) {
+        return "translate(" + (d.bbox.x - 2) + "," + d.bbox.y + ")";
+      })
+      .attr("width", function (d) {
+        return d.bbox.width + 4;
+      })
+      .attr("height", function (d) {
+        return d.bbox.height;
+      });
+
     d3.select(this.legendElement.nativeElement)
       .selectAll('ul')
       .remove();
-    
+
     // build the map legend
     var legend = d3.select(this.legendElement.nativeElement)
-        .append('ul')
-        .attr('class', 'list-inline');
+      .append('ul')
+      .attr('class', 'list-inline');
 
     var keys = legend.selectAll('li.key')
-        .data(this.color.range());
+      .data(this.color.range());
 
     var legend_items = ["Low", "", "", "", "", "", "", "", "High"];
 
     keys.enter().append('li')
-        .attr('class', 'key')
-        .style('border-top-color', String)
-        .text(function (d, i) {
-            return legend_items[i];
-        });
+      .attr('class', 'key')
+      .style('border-top-color', String)
+      .text(function (d, i) {
+        return legend_items[i];
+      });
   }
 
- 
-
   getTextBox(selection) {
-    selection.each(function(d) {
+    selection.each(function (d) {
       d.bbox = this.getBBox();
     });
   }
